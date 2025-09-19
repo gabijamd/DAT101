6d17dc872e4e6d39946e96f44bb1a1319ccc4f26
gabijamd <gabija@uia.no>
2025-09-19 20:08:48 +0200

    Task 3.1

--- module-3/lecture_01.mjs
+++ module-3/lecture_01.mjs
@@ -1,2 +1,64 @@
 "use strict"
 import {printOut} from "../../common/script/utils.mjs";
+
+/*const textFalse = "Detter er usant";
+const textTrue= " Dette er sant"; 
+
+const ageMovie =  16; 
+let age = 14; 
+
+if(age > ageMovie ) {
+    printOut(textTrue); 
+}
+else{
+    printOut(textFalse); 
+} */
+
+
+/*const text1 = "Gratulerer du er akkurat gammel nok!";
+const text2= " Du kan gå på kino"; 
+const text3 = "Beklager, du er ikke gammel nok:("
+
+const ageMovie =  16; 
+let age = 14; 
+
+if(age == ageMovie ) {
+    printOut(text1); 
+}
+else if ( age > ageMovie) {
+    printOut(text2); 
+}
+else  {
+    printOut(text3)
+}*/
+
+
+const text1 = " Du er gammelt til å ta med en som er ikke gammel nok";
+const text2= " Du kan gå på kino"; 
+const text3 = " Beklager, du er ikke gammel nok:("
+
+const ageMovie =  16; 
+const ageBringAlong =18; 
+let age1 = 14; // Gammel nok til å ha med seg en person under 16.
+let age2 = 19; // Personen er ikke gammelt notk til å gå alene. 
+
+if(age1 < age2){
+    const age1Old = age1; 
+    age1 = age2; 
+    age2 = age1Old; 
+}
+
+if(age1 >= ageMovie) {
+    printOut("Person 1 " + text2); 
+    if(age2 >= ageMovie) {
+        printOut(text2);
+    }else if (age1 >= ageBringAlong) { 
+        printOut ("Person 1" + text1); 
+        printOut ("Person 2" + text2); 
+    }else {
+        printOut("Person 2" + text3); 
+    }
+} else {
+    printOut("Vi har ingen case for dette!"); 
+}
+
--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -4,39 +4,120 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
+
+const yesBus = "I can catch the bus to school"; 
+const noBus = "I have to take car to school"; 
+const train = "I can take the train to school"
+
+let wakeUpTime1 = 7;
+let busTime = 7; 
+
+if (wakeUpTime1 == busTime ) {
+  printOut("The time is " + wakeUpTime1 + ". " + yesBus);
+}else if (wakeUpTime1 == 8 ) {
+  printOut("The time is " + wakeUpTime1 + ". " + train); 
+}else {
+  printOut("The time is " + wakeUpTime1 + ". " + noBus); 
 }
+
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const negativeIV = "Negative";
+const positiveIV = "Positive"; 
+const zeroIV = "0"
+
+let givenNumber = 5; 
+
+if ( givenNumber == 0) {
+  printOut("The given number is " + zeroIV); 
+}
+else if(givenNumber <0){
+  printOut("The given number " + givenNumber +" is "+ negativeIV);
+} else {
+  printOut ("The given number " + givenNumber +" is "+ positiveIV); 
+}
+
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
+printOut("--- Part 6, 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
 
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+const goodSize = "Thank you"; 
+const smallSize = "The image is too small";
+const largeSize = "The image is too large"; 
+
+let imageSize = Math.floor(Math.random() * 8) + 1; 
+
+if(imageSize > 6){
+  printOut("The photo size is " + imageSize + " , "+  largeSize); 
+} else if (imageSize > 3){
+  printOut("The photo size is " + imageSize + " , "+ goodSize);
+} else {
+  printOut("The photo size is " + imageSize + " , "+ smallSize);
+}
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const monthList =["January", "February", "Mars", "April", "Mai",
+"Jun", "Juli", "August", "September", "October", "November", "December"];
+const noOfMonth = monthList.length;
+const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+
+const daysInMonth = {
+    "January": 31,
+    "February": 28,
+    "Mars": 31,       
+    "April": 30,
+    "Mai": 31,        
+    "Juni": 30,
+    "Juli": 31,       
+    "August": 31,
+    "September": 30,
+    "October": 31,
+    "November": 30,
+    "December": 31
+};
+
+let rMonth = "You must take vitamin D";
+let notRMonth = "You do not need to take vitamin D";
+
+if(monthName.includes("r")) {
+  printOut("Month is " + monthName + " , "+  rMonth);
+} else {
+  printOut ("Month is " + monthName + " , "+ notRMonth);
+}
+
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+printOut( "In " + monthName + " and there are " + daysInMonth[monthName] +  " days" );
+
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let openMonth = "The gallery is open in";
+let closedMonth = "The gallery is closed in"; 
+let closedApr = "The gallery is closed, but we have temporary premises in the building next door in"; 
+
+
+if(monthName.includes("Mai")|| monthName.includes("Mars")){
+  printOut(closedMonth);
+} else if(monthName.includes("April")) {
+  printOut(closedApr);
+} else {
+  printOut(openMonth);
+}
+
+printOut( monthName  );
 printOut(newLine);
