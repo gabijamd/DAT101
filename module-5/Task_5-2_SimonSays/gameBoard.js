5cf6c3d2bde9d7214fba253ebf8a0de266b1b066
gabijamd <gabija@uia.no>
2026-01-27 13:10:18 +0100

    Simon says

---
+++ module-5/Task_5-2_SimonSays/gameBoard.js
@@ -0,0 +1,14 @@
+"use strict"
+import { TSprite } from "libSprite"; 
+
+
+export class TGameBoard extends TSprite {
+
+
+    constructor(aSpcvs, aSPI) {
+        super(aSpcvs, aSPI.Background, 0, 0); 
+    }
+
+
+
+}
