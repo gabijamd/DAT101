9e1c45c127e14f3e7ae98acbbfee32965ef00e53
gabijamd <gabija@uia.no>
2026-03-17 15:56:07 +0100

    drag-splice

--- module-5/Task_5-5_Mastermind/colorpicker.js
+++ module-5/Task_5-5_Mastermind/colorpicker.js
@@ -1,14 +1,19 @@
 "use strict";
 
-import { SpriteInfoList, spcvs } from "./Mastermind.mjs";
+import { SpriteInfoList, spcvs, colorPickers} from "./Mastermind.mjs";
 import { MastermindBoard } from "./MastermindBoard.mjs";
 import { TSpriteDraggable, TSnapTo } from "libSprite";
 
 export class TColorPicker extends TSpriteDraggable{
-    #colorPicker
 
     constructor(aPos){
         super(spcvs, SpriteInfoList.ColorPicker, aPos.x, aPos.y); 
+    }
+
+    onStartDrag(){
+        const index = colorPickers.indexOf(this); 
+        colorPickers.splice(index, 1);
+        colorPickers.push(this); 
     }
 
 }// End of TColorPicker
